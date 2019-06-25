79638cd066ee53550ee2e3c419d3b08b893a595d
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-24 23:24:35 -0300

    Adding .env.example

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-cycle */
 import axios from 'axios';
+import { REACT_APP_API_URL } from 'react-native-dotenv';
 
 import store from '../store';
 
 const api = axios.create({
-  baseURL: 'http://a5b07bef.ngrok.io',
+  baseURL: REACT_APP_API_URL,
 });
 
 api.interceptors.request.use((config) => {
